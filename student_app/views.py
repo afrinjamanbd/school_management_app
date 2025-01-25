ff72f737230fcb8b1daf2d9fa905c0c69ad26456
SakibSamiul <sb.sakib77@gmail.com>
2025-01-25 23:29:06 +0600

    Updated all CRUD methods for classes in views.py in the student_app.

--- student_app/views.py
+++ student_app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
-from django.db.models import Count, Avg
+from django.db.models import Avg
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Teacher, ClassNSection, Student, Subjects
 from .serializers import TeacherSerializer, ClassNSectionSerializer, StudentSerializer, SubjectsSerializer, InfoSerializer
-
+ 
 def home(request):
     return JsonResponse({'status': "Success"})
 
@@ -64,18 +64,60 @@ def model_filter():
 class InfoAPIView(APIView):
     def get(self, request, pk=None):
         if pk == 'all':
-            teachers = Teacher.objects.annotate(average_students=Avg('classnsection__total_student'))
+            teachers=Teacher.objects.annotate(average_students=Avg('classnsection_total_student'))
             serializer = InfoSerializer(teachers, many=True)
             return Response(serializer.data)
-        
         try:
-            teacher = Teacher.objects.prefetch_related('classnsection_set').get(pk=pk)
+            teachers = Teacher.objects.prefetch_related('classnsection_set').get(pk=pk)
         except Teacher.DoesNotExist:
-            return Response({'error': 'Teacher not found'}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = InfoSerializer(teacher)
+            return Response({'error': 'Teacher not found'}, status= status.HTTP_404_NOT_FOUND)
+        
+        serializer = InfoSerializer(teachers)
         return Response(serializer.data)
 
+    def post(self, request, pk=None):
+        try:
+            teachers = Teacher.objects.get(pk=pk)
+        except Teacher.DoesNotExist:
+            return Response({'error':'Teacher not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = TeacherSerializer(teachers, data=request.data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk):
+        try:
+            teachers = Teacher.objects.get(pk=pk)
+        except Teacher.DoesNotExist:
+            return Response({'error':'Teacher not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = TeacherSerializer(teachers, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request, pk):
+        try:
+            teachers = Teacher.objects.get(pk=pk)
+        except Teacher.DoesNotExist:
+            return Response({'error':'Teacher not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = TeacherSerializer(teachers, data=request.data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        try:
+            teacher = Teacher.objects.get(pk=pk)
+        except Teacher.DoesNotExist:
+            return Response({'error': 'Teacher not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        teacher.delete()
+        return Response({'message': 'Teacher deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
 
 class TeacherAPIView(APIView):
     def get(self, request, pk):
@@ -129,11 +171,19 @@ class TeacherAPIView(APIView):
         
         teacher.delete()
         return Response({'message': 'Teacher deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
-
+    
 class ClassNSectionAPIView(APIView):
-    def get(self, request):
-        sections = ClassNSection.objects.all()
-        serializer = ClassNSectionSerializer(sections, many=True)
+    def get(self, request, pk):
+        if pk == 'all':
+            sections = ClassNSection.objects.all()
+            serializer = ClassNSectionSerializer(sections, many=True)
+            return Response(serializer.data)
+        try:
+            sections = ClassNSection.objects.prefetch_related('class_section_api').get(pk=pk)
+        except ClassNSection.DoesNotExist:
+            return Response({'error': 'ClassNSection not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = ClassNSectionSerializer(sections)
         return Response(serializer.data)
 
     def post(self, request):
@@ -142,11 +192,47 @@ class ClassNSectionAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk):
+        try:
+            sections = ClassNSection.objects.get(pk=pk)
+        except ClassNSection.DoesNotExist:
+            return Response({'error': 'ClassNSection not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = ClassNSectionSerializer(sections, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request, pk):
+        try:
+            sections = ClassNSection.objects.get(pk=pk)
+        except ClassNSection.DoesNotExist:
+            return Response({'error': 'ClassNSection not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = ClassNSectionSerializer(sections, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        sections = ClassNSection.objects.get(pk=pk)
+        sections.delete()
+        return Response({'error': 'ClassNSection not found'}, status=status.HTTP_404_NOT_FOUND)
 
 class StudentAPIView(APIView):
-    def get(self, request):
-        students = Student.objects.all()
-        serializer = StudentSerializer(students, many=True)
+    def get(self, request, pk):
+        if pk == 'all':
+            students = Student.objects.all()
+            serializer = TeacherSerializer(students, many=True)
+            return Response(serializer.data)
+        try:
+            students = Student.objects.get(pk=pk)
+        except Teacher.DoesNotExist:
+            return Response({'error': 'Teacher not found'}, status=status.HTTP_404_NOT_FOUND)  
+        serializer = TeacherSerializer(students, many=False)
         return Response(serializer.data)
 
     def post(self, request):
@@ -155,6 +241,36 @@ class StudentAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk):
+        try:
+            student = Student.objects.get(pk=pk)
+        except Student.DoesNotExist:
+            return Response({'error': 'Students not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+    def patch(self, request, pk):
+        try:
+            student = Student.objects.get(pk=pk)
+        except Student.DoesNotExist:
+            return Response({'error': 'Students not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serailizer = StudentSerializer(student, data=request.data)
+        if serailizer.is_valid():
+            serailizer.save()
+            return Response(serailizer.data, status=status.HTTP_201_CREATED)
+        return Response(serailizer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        student = Student.objects.get(pk=pk)
+        student.delete()
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
 class SubjectsAPIView(APIView):
     def get(self, request):
@@ -162,11 +278,34 @@ class SubjectsAPIView(APIView):
         serializer = SubjectsSerializer(subjects, many=True)
         return Response(serializer.data)
 
-    def post(self, request):
-        serializer = SubjectsSerializer(data=request.data)
+    def post(self, request, pk):
+        try: 
+            subjects = Subjects.objects.get(pk=pk)     
+        except Subjects.DoesNotExist:
+            return Response({'errors':'Subjects not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = SubjectsSerializer(subjects, data=request.data)
+            
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def patch(self, request, pk):
+        try:
+            subjects = Subjects.objects.get(pk=pk)
+        except Subjects.DoesNotExist:
+            return Response({'errors':'Subjects not found'}, status = status.HTTP_404_NOT_FOUND)
+        
+        serializer = SubjectsSerializer(subjects, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        subjects = Subjects.objects.get(pk=pk)
+        subjects.delete()
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
